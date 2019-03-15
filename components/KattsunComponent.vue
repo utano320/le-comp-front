e03212654e57bf1b0e20001fff663aec826839cd
Mitsuo Utano <utano320@gmail.com>
2019-03-15 21:00:00 +0900

    kattusnさん作業会

---
+++ components/KattsunComponent.vue
@@ -0,0 +1,54 @@
+<template>
+   <div class="container">
+    <div class="row">
+      <select class="kattsun-component column" v-model="name">
+        <option value="">--- 選択してください ---</option>
+        <option
+          v-for="name in strength"
+          :value="name"
+          :key="name"
+        >
+          {{ name }}
+        </option>
+      </select>
+      <button @click="getCount(name)">実行</button>
+    </div>
+    <table>
+      <tr>
+        <th>順位</th>
+        <th>人数</th>
+      </tr>
+     <tr
+        v-for="(count, index) in stCount"
+        :key="index"
+      >
+        <td>{{ index + 1 }} 位</td>
+        <td>{{ count }} 人</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: ""
+    }
+  },
+  props: {
+    strength: {
+      type: Array,
+      default: []
+    },
+    getCount: {
+      type: Function,
+      default: null
+    },
+    stCount: {
+      type: Array,
+      default: []
+    }
+  }
+}
+</script>
---
+++ pages/kattsun.vue
@@ -0,0 +1,87 @@
+<template>
+ <div id="app">
+    <h1>Kattsun</h1>
+
+    <kattsun-component
+      :strength="strength"
+      :get-count="getCount"
+      :st-count="stCount"
+    />
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import axios from "axios";
+import KattsunComponent from "~/components/KattsunComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    KattsunComponent
+  },
+  data() {
+    return {
+      apiUrl: process.env.API_PROTOCOL + "://" + process.env.API_HOST,
+      strength: [
+        "アレンジ",
+        "回復志向",
+        "規律性",
+        "公平性",
+        "慎重さ",
+        "信念",
+        "責任感",
+        "達成欲",
+        "目標志向",
+        "活発性",
+        "競争性",
+        "コミュニケーション",
+        "最上志向",
+        "自我",
+        "自己確信",
+        "社交性",
+        "指令性",
+        "適応性",
+        "運命思考",
+        "成長促進",
+        "共感性",
+        "調和性",
+        "包含",
+        "個別化",
+        "ポジティブ",
+        "親密性",
+        "学習欲",
+        "原点思考",
+        "収集心",
+        "戦略性",
+        "着想",
+        "内省",
+        "分析思考",
+        "未来志向",
+      ],
+      stCount: []
+        };
+  },
+  methods: {
+
+    getCount(name) {
+      let self = this;
+      axios
+        .get(self.apiUrl + "/strength", {
+          params: {
+            name: name
+          }
+        })
+        .then(res => {
+          self.stCount = res.data.count;
+        });
+    }
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  color: tomato;
+}
+</style>
